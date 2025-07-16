60f4e57ccdb9e856220c34541662124dc034088b
Manuela Monteiro Fernandes de Oliveira <manuelamfo@ufmg.br>
2025-07-16 00:15:52 -0300

    rota api lanches

--- server.js
+++ server.js
@@ -92,5 +92,25 @@ app.post('/contato', (req, res) => {
             </footer>
         </body>
     </html>`);
+});
 
+app.get('/api/lanches', (req, res) => {
+    const lanches = [
+        {
+            "id": 1,
+            "nome": "DevBurger Clássico",
+            "ingredientes": "Pão brioche, Carne 150g, Queijo cheddar, Alface americana, Tomate fresco, Molho especial"
+        },
+        {
+            "id": 2,
+            "nome": "Burger de Bacon",
+            "ingredientes": "Pão australiano, Carne 180g, Queijo prato, Bacon crocante, Cebola caramelizada, Molho barbecue"
+        },
+        {
+            "id": 3,
+            "nome": "Commit Veggie",
+            "ingredientes": "Pão integral, Burger de grão de bico, Queijo vegano, Rúcula, Tomate seco, Maionese de ervas"
+        },
+    ];
+    res.json(lanches);
 });
